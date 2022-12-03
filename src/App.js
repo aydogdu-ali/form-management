25ade3393e488f8233dff1914e38f56b29144451
Ali <ali.ozgur@example.com>
2022-12-03 04:14:05 +0300

    created components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Register from './component/register/Register';
+import Container from "react-bootstrap/Container"
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
+    <Container> 
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1 className='text-danger'>Hoşgeldiniz</h1>
+
       </header>
+      <Button variant="success"> Hadi Bize Katılmak İçin Tıkla</Button>
+      <Register/>
     </div>
+    </Container>
   );
 }
 
---
+++ src/component/register/Register.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import RegisterStyle from "./Register.module.css"
+
+
+const Register = () => {
+  return (
+        <Container   className={RegisterStyle["register"]}> 
+           <Form>
+           <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Adınız:</Form.Label>
+        <Form.Control type="text" placeholder="Adınız" required />
+        
+      </Form.Group>
+
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Soy Adınız:</Form.Label>
+        <Form.Control type="text" placeholder="Soy Adınız"  required/>
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Telefon Numaranız:</Form.Label>
+        <Form.Control type="tel" placeholder="Telefon Numaranız" pattern="[0-9]{3}-[0-9]{3}-[0-9]{2} [0-9]{2}" required/>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Yaşadığınız Şehir:</Form.Label>
+        <Form.Control type="text" placeholder="Yaşadığınız Şehir" required/>
+      </Form.Group>
+
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address:</Form.Label>
+        <Form.Control type="email" placeholder="Enter email"required />
+        
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control type="password" placeholder="Password" required/>
+      </Form.Group>
+      
+      
+      <Button variant="info"  className="w-100 mt-3 mb-3"  type="submit">
+        Onay
+      </Button>
+    </Form>
+    </Container>
+
+    
+      );
+    }
+    
+ 
+  
+
+
+export default Register

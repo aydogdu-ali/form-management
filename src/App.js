8990cb77699141fe0a1dbcb9bfbbf8d8cce14391
Ali <ali.ozgur@example.com>
2022-12-04 03:08:37 +0300

    edit register component

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Register from './component/register/Register';
 import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button";
 
+
 function App() {
   return (
     <Container> 
     <div className="App">
       <header className="App-header">
        <h1 className='text-danger'>Hoşgeldiniz</h1>
+     
 
       </header>
       <Button variant="success"> Hadi Bize Katılmak İçin Tıkla</Button>
       <Register/>
     </div>
+  
     </Container>
   );
 }

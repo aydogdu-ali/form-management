ea5f948053dc190b4bbff2030ca5ef309f5d673c
Ali <ali.ozgur@example.com>
2022-12-08 14:27:01 +0300

    edit for run build

--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -7,6 +7,7 @@ import { LoginContext } from "../../contex/LoginContex";
 const Main = () => {
 
   const { login } = useContext(LoginContext);
+  
 
   // Üye olmayan kullanıcıyı register sayfasına yönlendirdim
   const navigate = useNavigate();
@@ -14,20 +15,26 @@ const Main = () => {
   //Kullanıcı butona tıkladığında register sayfasına yönlendirecek mouse eventini tanımladım.
   const handleClick = () => {
     navigate("/register");
+   
   };
 
   return (
     <>
       <header className={MainStyle["mid"]}>
         <h1 className="text-danger fs-bond">Hoşgeldiniz</h1>
-        {/*Kullanıcı login değil ise  görünecek metin*/}
+       
         {!login && (<Button variant="success " className="w-50" onClick={handleClick}>
          
          Hadi Bize Katılmak İçin Tıkla
         </Button>)}
 
-        {/*Kullanıcı login ise görünecek metin*/}
-        {login &&(<h4 className=" bg-dark text-white border rounded p-3 mt-2 ">"Geleceğin kapısı onu itmesini bilenlere açılır.” (Coluche) </h4>)}
+
+        {login && (<h4 className=" bg-dark text-white border rounded p-3 mt-2 ">"Geleceğin kapısı onu itmesini bilenlere açılır.” (Coluche) </h4>)}
+        
+        
+
+       
+        
       </header>
     </>
   );
